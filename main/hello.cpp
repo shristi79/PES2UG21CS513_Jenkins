b1a35c0ae966ef374b5d42a2375e3d5e8dccd9dd
shristi79 <124422063+shristi79@users.noreply.github.com>
2024-03-12 21:19:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "This is Keshav\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
